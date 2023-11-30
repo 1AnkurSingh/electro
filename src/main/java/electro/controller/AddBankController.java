aca56d35ba56e07e21d2a41a9cc1abff165cb08c
Ankur Singh <ankurrana231002@gmail.com>
2023-11-30 14:50:09 +0530

    Test Bank

--- src/main/java/electro/controller/AddBankController.java
+++ src/main/java/electro/controller/AddBankController.java
@@ -4,11 +4,13 @@ import electro.model.AddBank;
 import electro.repository.AddBankRepository;
 import electro.service.AddBankService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class AddBankController {
     @Autowired
     AddBankService addBankService;
