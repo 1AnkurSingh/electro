2e4b6cf19f26859985fb021b16285c9e9b2c2db9
Ankur Singh <ankurrana231002@gmail.com>
2023-11-30 16:11:58 +0530

    Completely running

--- src/main/java/electro/model/AddBank.java
+++ src/main/java/electro/model/AddBank.java
@@ -22,6 +22,6 @@ public class AddBank {
     private String holderName;
     private String bankAccountNumber;
     private String bankIFSC;
-    private String UserId;
+    private String userId;
 
 }
