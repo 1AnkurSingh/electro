500d23add31af3ab0e60f3817b552a245e65f343
Ankur Singh <ankurrana231002@gmail.com>
2023-12-02 10:07:44 +0530

    add 5 gr 30 min

--- src/main/java/electro/controller/WithdrawController.java
+++ src/main/java/electro/controller/WithdrawController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,6 +37,7 @@ public class WithdrawController {
         List<Withdraw> withdrawData = withdrawService.getWithdrawDataByUserId(userId);
 
         ZoneId clientZoneId = ZoneId.of(clientTimeZone);
+        ZoneOffset offset = ZoneOffset.of("+05:30"); // Offset for India Standard Time
 
         List<Map<String, Object>> formattedWithdrawData = new ArrayList<>();
 
@@ -42,8 +45,12 @@ public class WithdrawController {
             Map<String, Object> formattedWithdraw = new HashMap<>();
             formattedWithdraw.put("id", withdraw.getId());
 
-            LocalDateTime timestampIST = withdraw.getTimestamp().atZone(clientZoneId).toLocalDateTime();
-            formattedWithdraw.put("timestamp", timestampIST.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm:ss")));
+            LocalDateTime timestampUTC = withdraw.getTimestamp().atZone(ZoneOffset.UTC).toLocalDateTime();
+            LocalDateTime timestampIST = timestampUTC.plusHours(5).plusMinutes(30); // Adding 5 hours and 30 minutes
+
+            // Adjusting for client's time zone
+            ZonedDateTime zonedDateTimeIST = timestampIST.atZone(ZoneOffset.UTC).withZoneSameInstant(clientZoneId);
+            formattedWithdraw.put("timestamp", zonedDateTimeIST.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm:ss")));
 
             formattedWithdraw.put("amount", withdraw.getAmount());
             formattedWithdraw.put("userId", withdraw.getUserId());
@@ -52,6 +59,7 @@ public class WithdrawController {
 
         return formattedWithdrawData;
     }
+
 
 
 
